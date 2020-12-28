717a8aab2c956fce6cc218dc136930f472bbbfdb
Robert Hajbok <hajbok.robert@gmail.com>
2020-12-28 20:50:21 +0000

    Add project files.

---
+++ Cordova Res Images/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace ResizeImagesCordova
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ofd_Image = new System.Windows.Forms.OpenFileDialog();
+            this.pb_Image = new System.Windows.Forms.PictureBox();
+            this.btn_ChooseFile = new System.Windows.Forms.Button();
+            this.btn_Create = new System.Windows.Forms.Button();
+            this.rb_Icons = new System.Windows.Forms.RadioButton();
+            this.rb_ScreensPortrait = new System.Windows.Forms.RadioButton();
+            this.rbScreensLandscape = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Image)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb_Image
+            // 
+            this.pb_Image.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb_Image.Location = new System.Drawing.Point(12, 12);
+            this.pb_Image.Name = "pb_Image";
+            this.pb_Image.Size = new System.Drawing.Size(320, 320);
+            this.pb_Image.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_Image.TabIndex = 0;
+            this.pb_Image.TabStop = false;
+            // 
+            // btn_ChooseFile
+            // 
+            this.btn_ChooseFile.Location = new System.Drawing.Point(12, 361);
+            this.btn_ChooseFile.Name = "btn_ChooseFile";
+            this.btn_ChooseFile.Size = new System.Drawing.Size(75, 23);
+            this.btn_ChooseFile.TabIndex = 1;
+            this.btn_ChooseFile.Text = "Choose File";
+            this.btn_ChooseFile.UseVisualStyleBackColor = true;
+            this.btn_ChooseFile.Click += new System.EventHandler(this.btn_ChooseFile_Click);
+            // 
+            // btn_Create
+            // 
+            this.btn_Create.Enabled = false;
+            this.btn_Create.Location = new System.Drawing.Point(257, 361);
+            this.btn_Create.Name = "btn_Create";
+            this.btn_Create.Size = new System.Drawing.Size(75, 23);
+            this.btn_Create.TabIndex = 2;
+            this.btn_Create.Text = "Create";
+            this.btn_Create.UseVisualStyleBackColor = true;
+            this.btn_Create.Click += new System.EventHandler(this.btn_Create_Click);
+            // 
+            // rb_Icons
+            // 
+            this.rb_Icons.AutoSize = true;
+            this.rb_Icons.Checked = true;
+            this.rb_Icons.Location = new System.Drawing.Point(12, 338);
+            this.rb_Icons.Name = "rb_Icons";
+            this.rb_Icons.Size = new System.Drawing.Size(51, 17);
+            this.rb_Icons.TabIndex = 3;
+            this.rb_Icons.TabStop = true;
+            this.rb_Icons.Text = "Icons";
+            this.rb_Icons.UseVisualStyleBackColor = true;
+            // 
+            // rb_ScreensPortrait
+            // 
+            this.rb_ScreensPortrait.AutoSize = true;
+            this.rb_ScreensPortrait.Location = new System.Drawing.Point(88, 338);
+            this.rb_ScreensPortrait.Name = "rb_ScreensPortrait";
+            this.rb_ScreensPortrait.Size = new System.Drawing.Size(105, 17);
+            this.rb_ScreensPortrait.TabIndex = 4;
+            this.rb_ScreensPortrait.TabStop = true;
+            this.rb_ScreensPortrait.Text = "Screens (portrait)";
+            this.rb_ScreensPortrait.UseVisualStyleBackColor = true;
+            // 
+            // rbScreensLandscape
+            // 
+            this.rbScreensLandscape.AutoSize = true;
+            this.rbScreensLandscape.Location = new System.Drawing.Point(210, 338);
+            this.rbScreensLandscape.Name = "rbScreensLandscape";
+            this.rbScreensLandscape.Size = new System.Drawing.Size(122, 17);
+            this.rbScreensLandscape.TabIndex = 5;
+            this.rbScreensLandscape.TabStop = true;
+            this.rbScreensLandscape.Text = "Screens (landscape)";
+            this.rbScreensLandscape.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 395);
+            this.Controls.Add(this.rbScreensLandscape);
+            this.Controls.Add(this.rb_ScreensPortrait);
+            this.Controls.Add(this.rb_Icons);
+            this.Controls.Add(this.btn_Create);
+            this.Controls.Add(this.btn_ChooseFile);
+            this.Controls.Add(this.pb_Image);
+            this.Name = "Form1";
+            this.Text = "Cordova Res Images";
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Image)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog ofd_Image;
+        private System.Windows.Forms.PictureBox pb_Image;
+        private System.Windows.Forms.Button btn_ChooseFile;
+        private System.Windows.Forms.Button btn_Create;
+        private System.Windows.Forms.RadioButton rb_Icons;
+        private System.Windows.Forms.RadioButton rb_ScreensPortrait;
+        private System.Windows.Forms.RadioButton rbScreensLandscape;
+    }
+}
+
---
+++ Cordova Res Images/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ResizeImagesCordova
+{
+    public partial class Form1 : Form
+    {
+        Image _image;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            ofd_Image.Filter = "";
+
+            var codecs = ImageCodecInfo.GetImageEncoders();
+            var sep = string.Empty;
+
+            foreach (var c in codecs)
+            {
+                var codecName = c.CodecName.Substring(8).Replace("Codec", "Files").Trim();
+                ofd_Image.Filter = string.Format("{0}{1}{2} ({3})|{3}", ofd_Image.Filter, sep, codecName, c.FilenameExtension);
+                sep = "|";
+            }
+
+            ofd_Image.Filter = string.Format("{0}{1}{2} ({3})|{3}", ofd_Image.Filter, sep, "All Files", "*.*");
+
+            ofd_Image.DefaultExt = ".png"; // Default file extension 
+        }
+
+        private void btn_ChooseFile_Click(object sender, EventArgs e)
+        {
+            var dr = ofd_Image.ShowDialog();
+            if (dr != DialogResult.OK) return;
+            try
+            {
+                _image = Image.FromFile(ofd_Image.FileName);
+            }
+            catch
+            {
+                MessageBox.Show("Please choose an image!");
+                return;
+            }
+            pb_Image.Image = _image;
+            btn_Create.Enabled = true;
+        }
+
+        private void btn_Create_Click(object sender, EventArgs e)
+        {
+            btn_Create.Enabled = false;
+
+            if (rb_Icons.Checked)
+            {
+                if (Directory.Exists("icons"))
+                    Directory.Delete("icons", true);
+                Directory.CreateDirectory("icons");
+                Directory.CreateDirectory("icons/android");
+                Directory.CreateDirectory("icons/ios");
+                Directory.CreateDirectory("icons/windows");
+                Directory.CreateDirectory("icons/wp8");
+
+                var iconDimensions = new Dictionary<string, Size>
+                {
+                    {"drawable-ldpi-icon", new Size(36, 36)},//android
+                    {"drawable-mdpi-icon", new Size(48, 48)},
+                    {"drawable-hdpi-icon", new Size(72, 72)},
+                    {"drawable-xhdpi-icon", new Size(96, 96)},
+                    {"drawable-xxhdpi-icon", new Size(144, 144)},
+                    {"drawable-xxxhdpi-icon", new Size(192, 192)},
+                    {"icon-small", new Size(29, 29)},//ios
+                    {"icon-40", new Size(40, 40)},
+                    {"icon-50", new Size(50, 50)},
+                    {"icon-57", new Size(57, 57)},
+                    {"icon-small-2x", new Size(58, 58)},
+                    {"icon-60", new Size(60, 60)},
+                    {"icon-72", new Size(72, 72)},
+                    {"icon-76", new Size(76, 76)},
+                    {"icon-40-2x", new Size(80, 80)},
+                    {"icon-50-2x", new Size(100, 100)},
+                    {"icon-57-2x", new Size(114, 114)},
+                    {"icon-60-2x", new Size(120, 120)},
+                    {"icon-72-2x", new Size(144, 144)},
+                    {"icon-76-2x", new Size(152, 152)},
+                    {"icon-60-3x", new Size(180, 180)},
+                    {"smalllogo", new Size(30, 30)},//windows
+                    {"storelogo", new Size(50, 50)},
+                    {"logo", new Size(150, 150)},
+                    {"icon-62-tile", new Size(62, 62)},//wp8
+                    {"icon-173-tile", new Size(173, 173)}
+                };
+
+                foreach (var dimension in iconDimensions)
+                {
+                    var imagePath = "";
+                    if (dimension.Key.StartsWith("draw"))
+                        imagePath = "icons/android/";
+                    else if (dimension.Key.EndsWith("logo"))
+                        imagePath = "icons/windows/";
+                    else if (dimension.Key.EndsWith("tile"))
+                        imagePath = "icons/wp8/";
+                    else if (dimension.Key.StartsWith("icon"))
+                        imagePath = "icons/ios/";
+
+                    Image imgGdi = ResizeImage(_image, dimension.Value.Width, dimension.Value.Height);
+                    imgGdi.Save(imagePath + dimension.Key + ".png", ImageFormat.Png);
+                }
+                MessageBox.Show("Icons successfully created!");
+                btn_Create.Enabled = true;
+            }
+            else if (rb_ScreensPortrait.Checked)
+            {
+                if (Directory.Exists("screens"))
+                    Directory.Delete("screens", true);
+                Directory.CreateDirectory("screens");
+                Directory.CreateDirectory("screens/android");
+                Directory.CreateDirectory("screens/ios");
+                Directory.CreateDirectory("screens/windows");
+                Directory.CreateDirectory("screens/wp8");
+
+                var screenDimensions = new Dictionary<string, Size>
+                {
+                    {"drawable-port-hdpi-screen", new Size(480, 800)}, //android
+                    {"drawable-port-ldpi-screen", new Size(240, 320)},
+                    {"drawable-port-mdpi-screen", new Size(320, 480)},
+                    {"drawable-port-xhdpi-screen", new Size(720, 1280)},
+                    {"drawable-port-xxhdpi-screen", new Size(960, 1600)},
+                    {"screen-ipad-portrait", new Size(768, 1024)}, //ios
+                    {"screen-iphone-568h-2x", new Size(640, 1136)},
+                    {"screen-iphone-portrait", new Size(320, 480)},
+                    {"screen-iphone-portrait-2x", new Size(640, 960)},
+                    {"screen-portrait", new Size(480, 800)}, //wp8
+                };
+
+                foreach (var dimension in screenDimensions)
+                {
+                    var imagePath = "";
+                    if (dimension.Key.StartsWith("draw"))
+                        imagePath = "screens/android/";
+                    else if (dimension.Key == "screen-portrait")
+                        imagePath = "screens/wp8/";
+                    else if (dimension.Key.StartsWith("screen"))
+                        imagePath = "screens/ios/";
+
+                    Image imgGdi = ResizeImage(_image, dimension.Value.Width, dimension.Value.Height);
+                    imgGdi.Save(imagePath + dimension.Key + ".png", ImageFormat.Png);
+                }
+                MessageBox.Show("Portrait screens successfully created!");
+                btn_Create.Enabled = true;
+            }
+            else
+            {
+                if (Directory.Exists("screens"))
+                    Directory.Delete("screens", true);
+                Directory.CreateDirectory("screens");
+                Directory.CreateDirectory("screens/android");
+                Directory.CreateDirectory("screens/ios");
+                Directory.CreateDirectory("screens/windows");
+                Directory.CreateDirectory("screens/wp8");
+
+                var screenDimensions = new Dictionary<string, Size>
+                {
+                    {"drawable-land-hdpi-screen", new Size(800, 480)}, //android
+                    {"drawable-land-ldpi-screen", new Size(320, 240)},
+                    {"drawable-land-mdpi-screen", new Size(480, 320)},
+                    {"screen-ipad-landscape", new Size(1024, 768)}, //ios
+                    {"screen-ipad-landscape-2x", new Size(2048, 1536)},
+                    {"splashscreen", new Size(620, 300)}, //windows
+                };
+
+                foreach (var dimension in screenDimensions)
+                {
+                    var imagePath = "";
+                    if (dimension.Key.StartsWith("draw"))
+                        imagePath = "screens/android/";
+                    else if (dimension.Key == "splashscreen")
+                        imagePath = "screens/windows/";
+                    else if (dimension.Key.StartsWith("screen"))
+                        imagePath = "screens/ios/";
+
+                    Image imgGdi = ResizeImage(_image, dimension.Value.Width, dimension.Value.Height);
+                    imgGdi.Save(imagePath + dimension.Key + ".png", ImageFormat.Png);
+                }
+                MessageBox.Show("Landscape screens successfully created!");
+                btn_Create.Enabled = true;
+            }
+        }
+
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+    }
+}
